ef0a4f2396a4fb5be6d2521d3eeb47b6b8accef0
khalidswe <khalid35-2520@diu.edu.bd>
2021-09-07 21:36:01 +0600

    added 2

---
+++ 2. data_var_type.py
@@ -0,0 +1,43 @@
+"""  #this is comment
+x = 5 + 2.23
+ print(x)
+
+ b = 10
+print (b*x)
+"""
+x = 4
+x = "start" #this line will print
+print(x)
+
+y = str(3)
+z = int(3)
+g = float(3)
+print( y,z,g)
+
+# for see type 
+print(type(x))
+print(type(y))
+print(type(z))                  
+print(type(g))
+
+k = "khalid" #single double quotes
+l = 'khalid'
+
+print(type(k))
+print(type(l)) #same output
+ 
+f = 4     #case-sensitive 
+F = "future" #F will not change coz of small letter and capital letter
+print(f,F)
+
+n , m = "name","valorant" #multiple value single line input
+o = p = "over power" #same value
+print(n,m,o,p) 
+
+game = ["valorant","csgo","gta"] #unpack collection
+v,cs,gt = game
+print(v,cs,gt)
+
+s , t = input().split() #split function
+print("this answer is : ",s,t)
+
